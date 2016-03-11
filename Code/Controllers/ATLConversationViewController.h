0044ebf038dff8bb3b556d33f3e88759d223cbdc
Pierre Dulac <dulacpier@gmail.com>
2016-03-11 12:01:13 +0100

    Remove the wrongly put nullable tag for the conversation property

--- Code/Controllers/ATLConversationViewController.h
+++ Code/Controllers/ATLConversationViewController.h
@@ -212,7 +212,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  @abstract The `LYRConversation` object whose messages will be displayed in the controller.
  */
-@property (nonatomic, nullable) LYRConversation *conversation;
+@property (nonatomic) LYRConversation *conversation;
 
 /**
  @abstract The `LYRQueryController` object managing data displayed in the controller.
