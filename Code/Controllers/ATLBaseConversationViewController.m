05b832b107394f7c894410a9cf236c767cb37352
Eliezer Talón <elitalon@gmail.com>
2016-09-27 16:45:30 +0200

    Bump version check for content flashing workaround

--- Code/Controllers/ATLBaseConversationViewController.m
+++ Code/Controllers/ATLBaseConversationViewController.m
@@ -18,10 +18,12 @@
 //  limitations under the License.
 //
 
-#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-
 #import "ATLBaseConversationViewController.h"
 #import "ATLConversationView.h"
+
+static inline BOOL atl_systemVersionLessThan(NSString * _Nonnull systemVersion) {
+    return [[UIDevice currentDevice].systemVersion compare:systemVersion options:NSOrderedAscending];
+}
 
 @interface ATLBaseConversationViewController ()
 
@@ -152,7 +154,7 @@ static CGFloat const ATLMaxScrollDistanceFromBottom = 150;
     [super viewWillDisappear:animated];
     
     self.messageInputToolbar.translucent = NO;
-    if (SYSTEM_VERSION_LESS_THAN(@"9.0")) {
+    if (atl_systemVersionLessThan(@"10.0")) {
         // Workaround for view's content flashing onscreen after pop animation concludes on iOS 8.
         BOOL isPopping = ![self.navigationController.viewControllers containsObject:self];
         if (isPopping) {
